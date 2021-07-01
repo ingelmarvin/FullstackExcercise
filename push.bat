964d720d1b5207771bcaa8f408e88eab5461da47
Marvin Ingel <ingelmarvin@gmail.com>
2021-07-01 22:16:32 +0200

    test

---
+++ push.bat
@@ -0,0 +1,19 @@
+@echo off
+title Push everything to GitHub
+
+IF %1.==. GOTO No1
+
+git add .
+echo added files
+git commit -m "%1"
+echo commited
+git push
+echo pushed
+GOTO End1
+
+:No1
+    echo commit message fehlt
+    GOTO End1
+
+:End1
+    echo finished
